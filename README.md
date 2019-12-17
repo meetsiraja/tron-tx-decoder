6e502618d4d0a3e716a38f1559bd860d6d6fee45
Meet Siraja <meet.s@yudiz.in>
2019-12-17 13:03:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+<p align="center">
+<img src="https://tron.network/static/images/logo.png" width=200 />
+</p>
+
+## tron-tx-decoder
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com) [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Lightweight utility for decoding function parameters  and function output from Tron blocklchain transactions.
+
+- Minimal dependencies.
+
+- Find the input parameter passed to function by Transaction Id.
+
+- Find the exact output returned from the function by Transaction Id.
+
+
+- _[Built with ethers.js](https://github.com/ethers-io/ethers.js/)_.
+
+## Installation
+
+```bash
+npm i tron-tx-decoder
+```
+
+### Initialization
+
+```js
+const TronTxDecoder = require('tron-tx-decoder');
+
+const decoder = new TronTxDecoder({ mainnet: true });
+// { mainnet: false } for testnet
+```
+
+#### Decode Input:
+Decode funciton input parameters by passing transaction id.
+```js
+async function decodeTxInput(txId){
+    const decodedInput = await decoder.decodeInputById(txId);
+    return decodedInput;
+}
+
+decodeTxInput("0c739761b34a891b3bbecd528302de827736660d76c62dacf8d3a9ebe7dade08").then(console.log);
+// { 
+//    methodName: 'submitTheGame',
+//    decodedInput: 
+//     [ { '_betId (uint256)': BigNumber { _hex: '0x265aa6' } },
+//       { 'freshHouseSeed (address)': '0x8F476d1155E9910A885Cae1c4396BDD392c3883E' },
+//       { 'rollResult (uint8)': 11 },
+//       { '_gameOutcome (uint8)': 1 } ] 
+// }
+```
+
+#### Decode Output:
+Decode funciton outputs by passing transaction id.
+
+```js
+async function decodeTxOutput(txId){
+    const decodedOutput = await decoder.decodeResultById(txId);
+    return decodedOutput;
+}
+
+decodeTxOutput("0c739761b34a891b3bbecd528302de827736660d76c62dacf8d3a9ebe7dade08").then(console.log);
+// { 
+//    methodName: 'submitTheGame',
+//    decodedOutput: 
+//      [ { uint256: BigNumber { _hex: '0x265aa6' } },
+//        { uint256: BigNumber { _hex: '0x0615ec20' } } ] 
+// }
+
+```
+
+
+
+
+#### BigNumber
+
+- BigNumber can be converted to number using  ***prototype*** .toNumber(). Which return a JavaScript number of the value.
+
+[BigNumber Documentation (ethers.js)](https://docs.ethers.io/ethers.js/html/api-utils.html#big-numbers)
+
+#### Licence
+
+[MIT](https://github.com/meetsiraja/tron-tx-decoder/LICENCE.md)
+
---
+++ decoder.js
@@ -0,0 +1,268 @@
+const axios = require('axios')
+const { utils } = require('ethers')
+
+const MAINNET_NODE = 'https://api.trongrid.io';
+const TESTNET_NODE = 'https://api.shasta.trongrid.io'
+
+class TronTxDecoder {
+
+    /**
+     * Create a TronTxDecoder object
+     *
+     * @param {Object} config the rootchain configuration object
+     * @param {Web3} config.web3 a web3 instance
+     * @param {string} config.plasmaContractAddress the address of the PlasmaFramework contract
+     * @return {TronTxDecoder} a TronWeb object
+     *
+     */
+    constructor ({ mainnet }) {
+        mainnet ? this.tronNode = MAINNET_NODE : this.tronNode = TESTNET_NODE;
+        // (this.tronWeb, this.tronNode) = initTronWeb(mainnet);
+    }
+
+    /**
+     * Decode result data from the transaction hash
+     *
+     * @method decodeResultById
+     * @param {string} transactionID the transaction hash
+     * @return {Object} decoded result with method name
+     */
+    async decodeResultById(transactionID){
+
+        try{
+            let transaction = await _getTransaction(transactionID, this.tronNode);
+            let data = '0x'+transaction.raw_data.contract[0].parameter.value.data;
+            let contractAddress = transaction.raw_data.contract[0].parameter.value.contract_address;
+            if(contractAddress === undefined)
+                throw 'No Contract found for this transaction hash.';
+            let abi = await _getContractABI(contractAddress, this.tronNode);
+
+            const resultInput = _extractInfoFromABI(data, abi);
+            let functionABI = abi.find(i => i.name === resultInput.method);
+
+            if(!functionABI.outputs)
+                return {
+                    methodName: resultInput.method,
+                    decodedOutput: {}
+                };
+            let outputType = functionABI.outputs;
+            const types = outputType.map(({type}) => type);
+            const names = resultInput.namesOutput;
+            
+            var encodedResult = await _getHexEncodedResult(transactionID, this.tronNode);
+            if(!encodedResult.includes('0x')){
+                let resMessage = "";
+                let i = 0, l = encodedResult.length;
+                for (; i < l; i += 2) {
+                    let code = parseInt(encodedResult.substr(i, 2), 16);
+                    resMessage += String.fromCharCode(code);
+                }
+
+                return {
+                    methodName: resultInput.method,
+                    decodedOutput: resMessage
+                };
+                
+            }
+           
+            var outputs = utils.defaultAbiCoder.decode(types, encodedResult);
+            let outputArray = new Array()
+            for(var i=0; i<types.length; i++){
+                let name, outputObject = {};
+                name = void 0 !== names[i] ? names[i]+" ("+types[i]+")" : types[i];
+                let output = outputs[i]
+                outputObject[name] = output;
+                outputArray.push(outputObject)
+            }
+            return {
+                methodName: resultInput.method,
+                decodedOutput: outputArray
+            };
+
+        }catch(err){
+            throw new Error(err)
+        }
+    }
+
+    /**
+     * Decode result data from the transaction hash
+     *
+     * @method decodeResultById
+     * @param {string} transactionID the transaction hash
+     * @return {Object} decoded result with method name
+     */
+    async decodeInputById(transactionID){
+
+        try{
+
+            let transaction = await _getTransaction(transactionID, this.tronNode);
+            let data = '0x'+transaction.raw_data.contract[0].parameter.value.data;
+            let contractAddress = transaction.raw_data.contract[0].parameter.value.contract_address;
+            if(contractAddress === undefined)
+                throw 'No Contract found for this transaction hash.';
+            let abi = await _getContractABI(contractAddress, this.tronNode);
+
+            const resultInput = _extractInfoFromABI(data, abi);
+            var names = resultInput.namesInput;
+            var inputs = resultInput.inputs;
+            var types = resultInput.typesInput;
+            let inputArray = new Array();
+            
+            for(var i=0; i<names.length; i++){
+                let inputObject = {};
+                let name = names[i] + ' (' + types[i]+ ')';
+                let input = inputs[i]
+                inputObject[name] = input;
+                inputArray.push(inputObject);
+            }
+            return {
+                methodName: resultInput.method,
+                decodedInput: inputArray
+            };
+
+        }catch(err){
+            throw new Error(err)
+        }
+    }
+}
+
+async function _getTransaction(transactionID, tronNode){
+    try{
+        const transaction = await axios.post(`${tronNode}/wallet/gettransactionbyid`, { value: transactionID});
+        if (!Object.keys(transaction.data).length)
+            throw 'Transaction not found';
+        return transaction.data
+    }catch(error){
+        throw error;
+    }
+}
+
+async function _getHexEncodedResult(transactionID, tronNode){
+    try{
+        const transaction = await axios.post(`${tronNode}/wallet/gettransactioninfobyid`, { value: transactionID});
+        if (!Object.keys(transaction.data).length)
+            throw 'Transaction not found';
+        if(transaction.data.contractResult[0] == '')
+            return transaction.data.resMessage;
+        return '0x'+transaction.data.contractResult[0]
+    }catch(error){
+        throw error;
+    }    
+}
+
+
+async function _getContractABI(contractAddress, tronNode){
+    try{
+        const contract = await axios.post(`${tronNode}/wallet/getcontract`, { value: contractAddress});
+        if (contract.Error)
+            throw 'Contract does not exist';
+        return contract.data.abi.entrys;
+    }catch(error){
+        throw error;
+    }
+}
+
+function _genMethodId (methodName, types) {
+    const input = methodName + '(' + (types.reduce((acc, x) => {
+      acc.push(_handleInputs(x))
+      return acc
+    }, []).join(',')) + ')'
+  
+    return utils.keccak256(Buffer.from(input)).slice(2, 10)
+}
+
+function _extractInfoFromABI(data, abi){
+
+    const dataBuf = Buffer.from(data.replace(/^0x/, ''), 'hex');
+
+    const methodId = Array.from(dataBuf.subarray(0, 4), function (byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+    
+    var inputsBuf = dataBuf.subarray(4);
+
+    return abi.reduce((acc, obj) => {
+        if (obj.type === 'constructor') return acc
+        if (obj.type === 'event') return acc
+        const method = obj.name || null
+        let typesInput = obj.inputs ? obj.inputs.map(x => {
+          if (x.type === 'tuple[]') {
+            return x
+          } else {
+            return x.type
+          }
+        }) : [];
+
+        let typesOutput = obj.outputs ? obj.outputs.map(x => {
+            if (x.type === 'tuple[]') {
+              return x
+            } else {
+              return x.type
+            }
+          }) : []
+
+        let namesInput = obj.inputs ? obj.inputs.map(x => {
+            if (x.type === 'tuple[]') {
+              return ''
+            } else {
+              return x.name
+            }
+        }) : [];
+
+        let namesOutput = obj.outputs ? obj.outputs.map(x => {
+            if (x.type === 'tuple[]') {
+                return ''
+            } else {
+                return x.name
+            }
+        }) : []
+        const hash = _genMethodId(method, typesInput)
+        if (hash === methodId) {
+            let inputs = []
+            
+            inputs = utils.defaultAbiCoder.decode(typesInput, inputsBuf);
+
+            return {
+              method,
+              typesInput,
+              inputs,
+              namesInput,
+              typesOutput,
+              namesOutput
+            }
+        }
+        return acc;
+    }, { method: null, typesInput: [], inputs: [], namesInput: [], typesOutput:[], namesOutput:[] });
+}
+
+function _handleInputs (input) {
+    let tupleArray = false
+    if (input instanceof Object && input.components) {
+        input = input.components
+        tupleArray = true
+    }
+
+    if (!Array.isArray(input)) {
+        if (input instanceof Object && input.type) {
+        return input.type
+        }
+
+        return input
+    }
+
+    let ret = '(' + input.reduce((acc, x) => {
+        if (x.type === 'tuple') {
+        acc.push(handleInputs(x.components))
+        } else if (x.type === 'tuple[]') {
+        acc.push(handleInputs(x.components) + '[]')
+        } else {
+        acc.push(x.type)
+        }
+        return acc
+    }, []).join(',') + ')'
+
+    if (tupleArray) {
+        return ret + '[]'
+    }
+}
+module.exports = TronTxDecoder;
